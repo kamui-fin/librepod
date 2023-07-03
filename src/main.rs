89ad5e41ae12e71a1daea15d325eefa4ac6db72e
Kamui <fin-kamui@pm.me>
2023-07-02 23:02:36 -0500

    feat: basic HTTP cache implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use chrono::{DateTime, TimeZone, Utc};
+use http::{request, response};
+use http_cache_semantics::{AfterResponse, BeforeRequest, CachePolicy, RequestLike, ResponseLike};
+use reqwest::header::{HeaderName, HeaderValue};
+use reqwest::{Client, Request, RequestBuilder, Response};
 use rss::{Channel, Enclosure, Item};
+use std::borrow::Cow;
+use std::rc::Rc;
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::{collections::HashMap, time::Instant};
 use uuid::Uuid;
 
 #[derive(Debug, Default, Clone)]
-struct Subscription {
+struct PodcastChannel {
     id: Uuid, // based on link
     rss_link: String,
     website_link: String,
@@ -19,7 +27,7 @@ struct Subscription {
 }
 
 #[derive(Debug, Default)]
-struct Episode {
+struct PodcastEpisode {
     id: Uuid, // uuid v5, based on guid -> link -> title -> throw out  ?
     title: Option<String>,
     link: Option<String>,
@@ -34,7 +42,7 @@ struct Episode {
     keywords: Option<String>,
     subtitle: Option<String>,
 
-    source: Subscription,
+    source: PodcastChannel,
 }
 
 fn parse_rfc2822_timestamp(timestamp: &str) -> Result<DateTime<Utc>, chrono::ParseError> {
@@ -57,7 +65,7 @@ fn hash_episode_id(item: &Item) -> Option<Uuid> {
     Some(uuid)
 }
 
-fn get_episode_data(channel: &Channel, rss_link: String) -> Vec<Episode> {
+fn get_episode_data(channel: &Channel, rss_link: String) -> Vec<PodcastEpisode> {
     let source = get_channel_data(channel, rss_link);
     channel
         .items()
@@ -73,7 +81,7 @@ fn get_episode_data(channel: &Channel, rss_link: String) -> Vec<Episode> {
             let content = item.content().map(ToString::to_string);
             let id = hash_episode_id(item)?;
 
-            let ep = Episode {
+            let ep = PodcastEpisode {
                 id,
                 title,
                 link,
@@ -82,7 +90,7 @@ fn get_episode_data(channel: &Channel, rss_link: String) -> Vec<Episode> {
                 pub_date,
                 enclosure,
                 source: source.clone(),
-                ..Episode::default()
+                ..PodcastEpisode::default()
             };
 
             if let Some(itunes) = item.itunes_ext() {
@@ -92,7 +100,7 @@ fn get_episode_data(channel: &Channel, rss_link: String) -> Vec<Episode> {
                 let subtitle = itunes.subtitle().map(ToString::to_string);
                 let content = itunes.summary().map(ToString::to_string);
 
-                Some(Episode {
+                Some(PodcastEpisode {
                     author,
                     image,
                     keywords,
@@ -107,7 +115,7 @@ fn get_episode_data(channel: &Channel, rss_link: String) -> Vec<Episode> {
         .collect()
 }
 
-fn get_channel_data(channel: &Channel, rss_link: String) -> Subscription {
+fn get_channel_data(channel: &Channel, rss_link: String) -> PodcastChannel {
     // collect all subscription metadata
     let website_link = channel.link().to_string();
     let title = channel.title().to_string();
@@ -123,7 +131,7 @@ fn get_channel_data(channel: &Channel, rss_link: String) -> Subscription {
     categories.sort();
     categories.dedup();
 
-    let sub = Subscription {
+    let sub = PodcastChannel {
         id: hash_subscription_id(channel),
         website_link,
         title,
@@ -131,7 +139,7 @@ fn get_channel_data(channel: &Channel, rss_link: String) -> Subscription {
         language,
         rss_link,
         image,
-        ..Subscription::default()
+        ..PodcastChannel::default()
     };
 
     if let Some(itunes) = channel.itunes_ext() {
@@ -153,7 +161,7 @@ fn get_channel_data(channel: &Channel, rss_link: String) -> Subscription {
             itunes_categories = categories;
         }
 
-        Subscription {
+        PodcastChannel {
             author,
             keywords,
             complete,
@@ -163,20 +171,84 @@ fn get_channel_data(channel: &Channel, rss_link: String) -> Subscription {
             ..sub
         }
     } else {
-        Subscription { categories, ..sub }
+        PodcastChannel { categories, ..sub }
     }
 }
 
-async fn process_rss(source: &str) -> Vec<Episode> {
-    let content = reqwest::get(source).await.unwrap().bytes().await.unwrap();
-    let channel = Channel::read_from(&content[..]).unwrap();
+fn update_request_parts(request: RequestBuilder, parts: request::Parts) -> RequestBuilder {
+    request.headers(parts.headers().clone())
+}
+
+fn update_response_parts(response: &mut Response, modified_parts: response::Parts) {
+    let headers = response.headers_mut();
+    for (key, value) in modified_parts.headers() {
+        headers.insert(key.clone(), value.clone());
+    }
+}
+
+async fn process_rss(
+    source: &str,
+    cache: &mut HashMap<String, (CachePolicy, Response)>,
+) -> Vec<PodcastEpisode> {
+    // before request
+    let client = Client::new();
+    let mut request_builder = client.get(source);
+    let mut orig_request = request_builder.try_clone().unwrap().build().unwrap();
+
+    let final_bytes;
+
+    // try to pull from cache if possible
+    if let Some((policy, cached_response)) = cache.get_mut(&source.to_string()) {
+        final_bytes = match policy.before_request(&orig_request, SystemTime::now()) {
+            BeforeRequest::Fresh(cached_response_parts) => {
+                println!("HIT CACHE!");
+                update_response_parts(cached_response, cached_response_parts);
+                cached_response.bytes().await.unwrap()
+            }
+            BeforeRequest::Stale { request, matches } => {
+                // update parts
+                request_builder = update_request_parts(request_builder, request);
+                orig_request = request_builder.try_clone().unwrap().build().unwrap();
+
+                let mut response = request_builder.send().await.unwrap();
+                match policy.after_response(&orig_request, &response, SystemTime::now()) {
+                    AfterResponse::NotModified(policy, parts) => {
+                        // 304
+                        // use cached body, update headers from parts
+                        println!("HIT CACHE!");
+                        update_response_parts(cached_response, parts);
+                        cached_response.bytes().await.unwrap()
+                    }
+                    AfterResponse::Modified(policy, parts) => {
+                        // 200
+                        // still have to update headers
+                        update_response_parts(&mut response, parts);
+                        // update body in cache
+                        cache.insert(source.to_string(), (policy, response));
+                        response.bytes().await.unwrap()
+                    }
+                }
+            }
+        }
+    } else {
+        let mut response = request_builder.send().await.unwrap();
+        let cache_policy = CachePolicy::new(&orig_request, &response);
+        if cache_policy.is_storable() {
+            println!("STORING IN CACHE");
+            cache.insert(source.to_string(), (cache_policy, response));
+        }
+        final_bytes = response.bytes().await.unwrap();
+    }
+
+    let channel = Channel::read_from(&final_bytes[..]).unwrap();
     get_episode_data(&channel, source.to_string())
 }
 
-async fn get_feed(sources: Vec<&str>) -> Vec<Episode> {
+async fn get_feed(sources: Vec<&str>) -> Vec<PodcastEpisode> {
+    let mut cache: HashMap<String, (CachePolicy, Response)> = HashMap::new();
     let mut feed = vec![];
     for source in sources {
-        feed.extend(process_rss(source).await);
+        feed.extend(process_rss(source, &mut cache).await);
     }
     feed.sort_by_key(|ep| ep.pub_date);
     feed.reverse();
@@ -187,15 +259,22 @@ async fn get_feed(sources: Vec<&str>) -> Vec<Episode> {
 async fn main() {
     // temporary testing
     // assume pubDate exists for now
+    let now = Instant::now();
     let sources = vec![
         "https://feeds.rebuild.fm/rebuildfm",
-        "https://getpodcast.xyz/data/ximalaya/12215723.xml",
+        "https://feeds.rebuild.fm/rebuildfm",
+        "https://feeds.rebuild.fm/rebuildfm",
+        "https://feeds.rebuild.fm/rebuildfm",
+        "https://feeds.rebuild.fm/rebuildfm",
+        /* "https://getpodcast.xyz/data/ximalaya/12215723.xml",
         "https://getpodcast.xyz/data/ximalaya/29161862.xml",
         "https://getpodcast.xyz/data/163/7.xml",
         "https://getpodcast.xyz/data/ximalaya/13773679.xml",
-        "https://tlxxfm.com/feed/podcast",
+        "https://tlxxfm.com/feed/podcast", */
     ];
     for ep in get_feed(sources).await {
         println!("{} {:#?}", ep.source.title, ep.pub_date.unwrap_or_default());
     }
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 }
